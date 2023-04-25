9c8f7d0139a03a3da4ff0f232a835bae063b9f49
Hector Ramos <hector.ramos@accionlabs.com>
2023-04-24 23:36:14 -0600

    feat: add db

---
+++ app/app/core/config.py
@@ -0,0 +1,58 @@
+import secrets
+
+from pydantic import BaseSettings, EmailStr
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    # 60 minutes * 24 hours * 8 days = 8 days
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+
+
+    POSTGRES_SERVER: str = "project-db"
+    POSTGRES_USER: str = "admin"
+    POSTGRES_PASSWORD: str = "Passw0rd"
+    POSTGRES_DB: str = "sk-project"
+    SQLALCHEMY_DATABASE_URI: str = "postgresql+psycopg2://admin:Passw0rd@host.docker.internal:5432/sk-project"
+
+    # @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    # def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+    #     if isinstance(v, str):
+    #         return v
+    #     return PostgresDsn.build(
+    #         scheme="postgresql",
+    #         user=values.get("POSTGRES_USER"),
+    #         password=values.get("POSTGRES_PASSWORD"),
+    #         host=values.get("POSTGRES_SERVER"),
+    #         path=f"/{values.get('POSTGRES_DB') or ''}",
+    #     )
+
+
+    # @validator("EMAILS_FROM_NAME")
+    # def get_project_name(cls, v: Optional[str], values: Dict[str, Any]) -> str:
+    #     if not v:
+    #         return values["PROJECT_NAME"]
+    #     return v
+
+    # EMAIL_RESET_TOKEN_EXPIRE_HOURS: int = 48
+    # EMAIL_TEMPLATES_DIR: str = "/app/app/email-templates/build"
+    # EMAILS_ENABLED: bool = False
+
+    # @validator("EMAILS_ENABLED", pre=True)
+    # def get_emails_enabled(cls, v: bool, values: Dict[str, Any]) -> bool:
+    #     return bool(
+    #         values.get("SMTP_HOST")
+    #         and values.get("SMTP_PORT")
+    #         and values.get("EMAILS_FROM_EMAIL")
+    #     )
+
+    FIRST_SUPERUSER: EmailStr = "project.admin@mock.com"
+    FIRST_SUPERUSER_PASSWORD: str = "Passw0rd"
+    USERS_OPEN_REGISTRATION: bool = False
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.api.api_v1.api import api_router
+from app.core.config import settings
 
-@app.get("/health")
-def health_check():
-    return {"payload": "OK"}
+app = FastAPI(
+    title="project-sk", openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+# Set all CORS enabled origins
+# if settings.BACKEND_CORS_ORIGINS:
+#     app.add_middleware(
+#         CORSMiddleware,
+#         allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+#         allow_credentials=True,
+#         allow_methods=["*"],
+#         allow_headers=["*"],
+#     )
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
